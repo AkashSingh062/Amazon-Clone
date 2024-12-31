df61518976265382689e2fc309c4e7613a2d9362
Akash Singh <akash622003singh@gmail.com>
2024-12-31 19:09:57 +0530

    error with the files

--- script/cart/cart.js
+++ script/cart/cart.js
@@ -1,21 +1,23 @@
 import { products } from "../../Data/products.js";
 
-const cartItems = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 1,
-  deliveryOptionId: '1'
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 2,
-  deliveryOptionId: '2'
-}];
+const cartItems = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 1,
+    deliveryOptionId: "1",
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 2,
+    deliveryOptionId: "2",
+  }
+];
 
-
-function renderCheckout (){
-  let cartHTML = '';
-cartItems.forEach(item => {
-  const product = products.find(p => p.id === item.productId);
-  cartHTML += `<div class="cart-items-container">
+function renderCheckout() {
+  let cartHTML = "";
+  cartItems.forEach((item) => {
+    const product = products.find((p) => p.id === item.productId);
+    cartHTML += `<div class="cart-items-container">
           <div class="delivery-date">
             Delivery date:
             <span class="delivery-date-value">Wednesday, January 8</span>
@@ -35,7 +37,7 @@ cartItems.forEach(item => {
                 <input type="number" class="new-quantity-input" />
                 <span class="update-quantity-link">Update</span>
                 <span class="save-quantity">Save</span>
-                <span class="delete-quantity">Delete</span>
+                <span class="delete-quantity" data-id="${product.id}">Delete</span>
               </div>
             </div>
             <div class="delivery-options">
@@ -82,9 +84,20 @@ cartItems.forEach(item => {
             </div>
           </div>
         </div>`;
-});
+  });
 
-document.querySelector('.cart-item-container1').innerHTML = cartHTML;
-};
+  document.querySelector(".cart-item-container1").innerHTML = cartHTML;
+}
 
 renderCheckout();
+
+document.querySelectorAll(".delete-quantity").forEach((del, idx) => {
+  del.addEventListener("click", () => {
+    
+    console.log(cartItems);
+    cartItems.splice(idx, 1);
+    console.log(cartItems);
+renderCheckout();
+    });
+  });
+
